d20732d36d04dc465d667d239c3ad77e3604171b
Komal Singh <65511621+KomalSingh21@users.noreply.github.com>
2023-01-28 00:33:25 +0530

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,7 +1,7 @@
 /** 
  * The FreeStyleProject class implements an application that simply prints "Hello Jenkins" to standard output.
  */
-public class FreeStyleProject {
+public class HelloJenkins {
   public static void main(String[] args) {
     System.out.println("Hello, Jenkins");
   }
